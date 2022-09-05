4abbc0f3129708245a232a82d07a4fbb74624fae
lukzuk01 <81394133+lukzuk01@users.noreply.github.com>
2022-09-04 21:05:25 -0300

    luis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,27 @@
 import { Component } from '@angular/core';
+
+interface Componente{
+  icon: string;
+  title: string;
+  url:string;
+
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
-    { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
-    { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
-    { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
-    { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
+  componentes : Componente[] =[
+ 
+    { title: 'Profesor', 
+    url: '/Registroprofe', 
+    icon: 'people-circle-outline' },
+
+    { title: 'Alumno', 
+    url: '/Registroalumno', 
+    icon: 'people-outline' },
+  ];
 }
